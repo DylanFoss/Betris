ed43a0994cfd575270781efd9093ccde9081eb0d
DylanFoss <57671477+DylanFoss@users.noreply.github.com>
2022-10-18 22:38:14 +0100

    Removed unecessary #includes from Game.h

--- Game.h
+++ Game.h
@@ -1,23 +1,6 @@
 #pragma once
-
-#include <array>
-#include <vector>
-#include <stack>
-#include <algorithm>
-
 #include "Renderer.h"
 #include "InputManager.h"
-#include "GameSettings.h"
-
-#include "Score.h"
-#include "ScoreCalculator.h"
-
-#include "Board.h"
-
-#include "Tetromino.h"
-#include "ActiveMino.h"
-#include "GhostMino.h"
-#include "Font.h"
 
 #include "GameStateEnum.h"
 #include "GameStateManager.h"
